92a7654be66e428259806378d4b106a8f6dba022
DmytroShmidt <67760955+DmytroShmidt@users.noreply.github.com>
2020-07-09 20:47:50 +0300

    Add files via upload

---
+++ First Project/js/main.js
@@ -0,0 +1,10 @@
+window.addEventListener("DOMContentLoaded", () => {
+  $(".carousel").slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    prevArrow:
+      '<div class="carousel_arrow carousel_prev"><i class="fas fa-chevron-left"></i></div>',
+    nextArrow:
+      '<div class="carousel_arrow carousel_next"><i class="fas fa-chevron-right"></i></div>',
+  });
+});
